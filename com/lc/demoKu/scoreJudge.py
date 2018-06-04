2b5b95a5e725635e20190d786eefb6f3532c1052
ahviplc <ahlc@sina.cn>
2018-06-04 14:04:44 +0800

    水仙花和分数判断 增加！2018年6月4日14:04:24！LC

---
+++ com/lc/demoKu/scoreJudge.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+score = int(input('输入分数:\n'))
+if score >= 90:
+    grade = 'A'
+elif score >= 60:
+    grade = 'B'
+else:
+    grade = 'C'
+
+print('%d 属于 %s' % (score, grade))
