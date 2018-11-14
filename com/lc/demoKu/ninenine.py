8fb68dc291487a278595ac9ca87316227d3100e1
ahviplc <ahlc@sina.cn>
2018-11-14 12:11:23 +0800

    加了用format格式化字符串，备注：Python用format格式化字符串 - Xjng - 博客园 http://www.cnblogs.com/Xjng/p/4092600.html !  LC  2018年11月14日12:11:09

--- com/lc/demoKu/ninenine.py
+++ com/lc/demoKu/ninenine.py
@@ -18,3 +18,13 @@ for i in range(1, 10):
     for j in range(1, i + 1):
         print('{}x{}={}\t'.format(i, j, i * j), end='')
     print()
+
+# 九九乘法表2 开始
+for i in range(1, 10):
+    for j in range(1, i + 1):
+        print('{0}x{1}={2}\t'.format(i, j, i * j), end='')
+    print()
+
+# Python用format格式化字符串 - Xjng - 博客园
+# http://www.cnblogs.com/Xjng/p/4092600.html
+# LC 2018年11月14日12:09:57
