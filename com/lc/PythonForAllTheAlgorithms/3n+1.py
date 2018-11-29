52be15c1f85669084b918e0ae398b0986144adf7
ahviplc <ahlc@sina.cn>
2018-11-29 15:35:56 +0800

    11:python全部算法 开源项目 完美！2：自己的一下小说明,小总结！ LC 2018年11月29日15:35:16

---
+++ com/lc/PythonForAllTheAlgorithms/3n+1.py
@@ -0,0 +1,30 @@
+from Maths.abs import absVal
+def main():
+    def n31(a):# a = initial number
+        c = 0
+        l = [a]
+        while a != 1:
+            if a % 2 == 0:#if even divide it by 2
+                a = a // 2
+            elif a % 2 == 1:#if odd 3n+1
+                a = 3*a +1
+            c += 1#counter
+            l += [a]
+
+        return l , c
+    print(n31(43))
+    print(n31(98)[0][-1])# = a
+    print(n31(13))
+    print("It took {0} steps.".format(n31(13)[1]))#optional finish
+
+if __name__ == '__main__':
+    main()
+
+    print(absVal(-1)) #测试from Maths.abs import absVal 可以run
+
+"""
+算法题：
+
+输入一个数，偶数时则砍掉一半；奇数时，则（3n+1）砍掉一半，最后直到得到1.问，进行了多少次？
+
+"""
