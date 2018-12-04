fe7bbcdfcda8e0c80277d1855451dbce15de0951
ahviplc <ahlc@sina.cn>
2018-12-04 17:06:50 +0800

    1:完善最大绝对值和最小绝对值，备注：是输出绝对值最大和最小的那个数本身。LC 2018年12月4日17:06:34

--- com/lc/PythonForAllTheAlgorithms/Maths/absMax.py
+++ com/lc/PythonForAllTheAlgorithms/Maths/absMax.py
@@ -1,29 +1,32 @@
-# from Maths.abs import absVal
 from abs import absVal
 
 def absMax(x):
     """
-    #>>>absMax([0,5,1,11])
+    >>absMax([0,5,1,11])
     11
     >>absMax([3,-10,-2])
     -10
     """
-    j = x[0]
+    j =x[0]
     for i in x:
-        if absVal(i) > j:
+        if absVal(i) > absVal(j):
             j = i
     return j
     #BUG: i is apparently a list, TypeError: '<' not supported between instances of 'list' and 'int' in absVal
     #BUG fix
 
 def main():
-    a = [3, 2, -11, -12]
-    print(absMax(a)) # = -11
+    a = [-13, 2, -11, -12]
+    print(absMax(a)) # = -13
 
 if __name__ == '__main__':
     main()
 
 """
 print abs Max
+
+absmax.py and absmin.py bugs fixed. I think absmax.py are the Numbers with the max output absolute value and the min output absolute value.
+And my version output example : absMax.py【a = [-13, 2, -11, -12] print(absMax(a)) # = -13】， this absMax print -13 ; 
+ please check it.
 
 """
--- com/lc/PythonForAllTheAlgorithms/Maths/absMin.py
+++ com/lc/PythonForAllTheAlgorithms/Maths/absMin.py
@@ -1,28 +1,27 @@
-# from Maths.abs import absVal
 from abs import absVal
-
 def absMin(x):
-
     """
-    # >>>absMin([0,5,1,11])
+    >>absMin([0,5,1,11])
     0
-    # >>absMin([3,-10,-2])
+    >>absMin([3,-10,-2])
     -2
     """
-    j = absVal(x[0])
+    j = x[0]
     for i in x:
-        if absVal(i) < j:
+        if absVal(i) < absVal(j):
             j = i
     return j
 
 def main():
-    a = [1,2,-11]
-    print(absMin(a)) # = 1
+    a = [-3,-1,2,-11]
+    print(absMin(a))  # = -1
 
 if __name__ == '__main__':
     main()
 
 """
 print abs Min
+absmax.py and absmin.py bugs fixed. I think absmin.py, are the Numbers with the max output absolute value and the min output absolute value.
+And my version output example : absMin.py【 a = [-3,-1,2,-11] print(absMin(a)) # = -1】， this absMin print -1 . please check it.
 
 """
