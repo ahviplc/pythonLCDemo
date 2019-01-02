989ac407a8180addc7de06ac0fdbb372cf06b757
LC <ahlc@sina.cn>
2019-01-02 17:12:52 +0800

    1:完善Flask搭建的一个简易的但是很全的博客系统!完善【blog\app\_init_.py】,废弃过时方法，使用正确的现在用法! LC 2019-1-2 17:12:45

--- com/lc/demo/flaskDemo/blog/app/__init__.py
+++ com/lc/demo/flaskDemo/blog/app/__init__.py
@@ -8,7 +8,8 @@ app = Flask(__name__)
 db = SQLAlchemy(app)
 app.config.from_object('config')
 lm = LoginManager()
-lm.setup_app(app)
+# lm.setup_app(app) # 过时方法 废弃使用 现在使用：lm.init_app(app)
+lm.init_app(app)
 lm.login_view = 'login'
 
 from app import views, models
