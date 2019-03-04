39d4dc847efd1f6a5d56fd5ac148c88fa820e17e
LC <ahlc@sina.cn>
2019-03-04 10:28:15 +0800

    1:update ServerApp.py,更全的，更完善的socketserver服务端demo! LC 2019-3-4 10:27:56

--- com/lc/demo/socketServerDemo/ServerApp.py
+++ com/lc/demo/socketServerDemo/ServerApp.py
@@ -17,18 +17,33 @@ LC博客url: http://oneplusone.top/index.html
 import socketserver
 
 
-class MyTCPHandler(socketserver.BaseRequestHandler):
+class MyTCPHandler(socketserver.BaseRequestHandler):  # 必须继承这个类才能实现并发
 
-    def handle(self):  # 所有请求的交互都是在handle里执行的,
+    def setup(self):
+        print("before handle,连接建立：", self.client_address)
+
+    def handle(self):  # 所有请求的交互都是在handle里执行的,每个链接建立后都会自动执行该方法
+        conn = self.request  # 这个是每个客户端的链接
+        conn.sendall(bytes('conn ok', encoding='utf-8'))  # conn.sendall('conn ok'.encode('utf-8'))
         while True:
             try:
                 self.data = self.request.recv(1024).strip()  # 每一个请求都会实例化MyTCPHandler(socketserver.BaseRequestHandler):
                 print("{} wrote:".format(self.client_address[0]))
                 print(self.data)  # print(self.data.decode('ascii'))  # 将bytes变为str 用decode()方法
                 self.request.sendall(self.data.upper()+'-LC'.encode('utf-8'))  # sendall是重复调用send.  # '-LC'.encode('utf-8')  # '-LC'.encode('ascii')
-            except ConnectionResetError as e:
-                print("err ", e)
+            except Exception as e:  # ConnectionError[ConnectionResetError,ConnectionAbortedError]
+                print(self.client_address, "连接断开-err:", e)
                 break
+            finally:
+                pass
+                # self.request.close()
+
+            # except ConnectionResetError as e:
+            #     print("err ", e)
+            #     break
+
+    def finish(self):
+        print("finish run after handle")
 
 
 if __name__ == "__main__":
