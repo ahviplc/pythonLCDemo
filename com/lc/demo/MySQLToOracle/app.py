c87bdc66bc4ff4cd7e6c6826ce50d0bcb63eaa3e
ahviplc <ahlc@sina.cn>
2021-04-06 10:07:20 +0800

    MySQLToOracle 引入tests包 所有的测试在这里了.

--- com/lc/demo/MySQLToOracle/app.py
+++ com/lc/demo/MySQLToOracle/app.py
@@ -15,61 +15,6 @@ from utils.start_to_end_time_consuming import start_and_end
 from utils.util import get_now_time
 
 
-# 可用
-# from config.config import mysql_db_config as conf
-
-def run_pymysql_dbhelper():
-    from core import pymysql_DBHelper as pd
-    db = pd.DBHelper('select * from settings')
-    # db.select()
-    # db.selectDict()
-    for i in [1, 3]:
-        db.selectDict_finally_close_db()
-    pass
-
-
-def run_cx_Oracle_dbhelper():
-    from core import cx_Oracle_DBHelper as cod
-    db = cod.DBHelper()
-    sql = "select * from BRANCH_INFO"
-    # 查询所有
-    # fc = db.select_all(sql)
-    # 带条件查询
-    # data = [{}] # 不传 代表只是sql 类似db.select_all(sql)
-    # fc = db.select_by_where_many_params_dict(sql, data)
-
-    sql2 = "select * from BRANCH_INFO where BI_ORG_ID= :bi_org_id"
-    # 带条件查询 含真实条件
-    bi_org_id = '0003'
-    data2 = [{"bi_org_id": bi_org_id}]  # 不传 代表只是sql 类似db.select_all(sql)
-    fc = db.select_by_where_many_params_dict(sql2, data2)
-    print(fc)
-    pass
-
-
-def run():
-    print(get_now_time())
-    pass
-
-
-def run_pony():
-    from core import pony_orm_DBHelper as pod
-    pod.run_pony()  # 必须要执行的
-    pod.add_one()
-    pod.run_db_session()
-
-
-def run_test():
-    # 引入测试逻辑服务类
-    from services import pony_orm_test_service as pots
-    from db import db
-    print('进行测试业务逻辑')
-    db.init_db()
-    pots.add_data_artist_for_mysql()
-    pots.add_data_artist_for_oracle()
-    pass
-
-
 def run_lmt():
     # 引入lmt逻辑服务类
     from services import pony_orm_lmt_service as pols
@@ -82,11 +27,6 @@ if __name__ == '__main__':
     # sys.stdout = PrintLogger('MySQLToOracle.app.log')  # 监听所有的print到log日志 封装类 如不需要打印所有输出print的log日志，隐掉这段即可
     start_and_end(True, 1)
     # ------------------------------------------
-    # run()
-    # run_pymysql_dbhelper()
-    # run_cx_Oracle_dbhelper()
-    # run_pony()
-    run_test()
-    # run_lmt()
+    run_lmt()
     # ------------------------------------------
     start_and_end(False, 1)
